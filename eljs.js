20d0057a570e75c71b333c6f8fcf6601edaee95a
elevenee <39453790+elevenee@users.noreply.github.com>
2018-05-20 14:35:00 +0700

    Create eljs.js

---
+++ eljs.js
@@ -0,0 +1,10 @@
+	var pres = document.getElementsByTagName("pre");
+for (var i = 0; i < pres.length; i++) {
+  pres[i].addEventListener("dblclick", function () {
+    var selection = getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(this);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }, false);
+}
